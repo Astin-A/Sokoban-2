8932a2eebde4a9ab7521ceb5189c66b4b7bf7fb7
AliAzarafza <astinazarafza@gmail.com>
2023-12-16 18:25:14 -0500

    Update main.c
    Change temp to load_map local variable
    Implementing player movement function

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <conio.h>
 #define true 1
 #define false 0
 
-char temp[5][30][30]; //* Variable for first reading the map file and storing the data
-char map[5][30][30]; //* Variable that processes and stores the temp variable
+
+
+char map[5][30][30]; //* Variable that processes and stores the temp variable in load_map
+char nowPlayMap[30][30] = {NULL, };
 int current_player_pos[2];
 
 
@@ -11,11 +14,13 @@ void load_map(void){ //* Function to load data from a map file, store it in temp
 
     FILE *ifp;
     /*
+     * temp: Variable that first reads the map file and stores the data
      * imap: map number
      * iy: Map row (Y-axis) number
      * ix: Column (X-axis) number of the map
      * will_load: Variable that sets whether to load characters
     */
+    char temp[5][30][30] = {NULL, };
     int imap = 0, iy = 0, ix = 0, c = 0;
     _Bool will_load = false;
     ifp = fopen("map", "r");
@@ -150,13 +155,35 @@ void get_player_pos(int imap)
 }
 
 
+void move_player(char c)
+{
+    //First, up, down, left and right
+    //First delete the location of the golbaengi in the map file
+    nowPlayMap[current_player_pos[0]][current_player_pos[1]] = '.';
+    switch (c)
+    {
+        case 'h':// left
+            current_player_pos[0]-=1;
+        case 'j':// under
+            current_player_pos[1]+=1
+        case 'k':// top
+            current_player_pos[1]-=1;
+        case 'l' :// right 
+            current_player_pos[0]+=1;
+    }
+    // Take a new picture of the golbaengi location
+    nowPlayMap[current_player_pos[0]][current_player_pos[1]] = '@';
+}
+
 
 
 int main(void)
 {
+    char command;
     load_map();
 
-      for (int imap = 0; imap <= 4; imap++)
+    /*
+    for (int imap = 0; imap <= 4; imap++)
     {
         get_player_pos(imap);
         printf("PLAYER_POSITION: %d, %d\n", current_player_pos[0], current_player_pos[1]);
@@ -171,7 +198,15 @@ int main(void)
     }
         printf("\n\n");
 }
+*/
+    while(1)
+    {
 
+        //* Assuming map file number 1
+        command = getch();
+        move_player(command);
+        //
+    }
 
 
     return 0;
