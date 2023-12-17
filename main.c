ea45493b78bd49692662debc13c936d37778a8f2
AliAzarafza <astinazarafza@gmail.com>
2023-12-17 13:38:23 -0500

    Basic undo implementation

--- main.c
+++ main.c
@@ -21,6 +21,7 @@ _Bool check_mapfile(int n,int m) //* Check the number of boxes and goal points i
 }
 
 void selectmap(int imap);
+void record_history(char move);
 
 int getch(void) //* Function for using getch() in Linux
 {
@@ -258,15 +259,19 @@ void move_player(char move,int imap) //* Function to move the player
     {
         case 'h':// left
             current_player_pos[0]-=1;
+            record_history('l');
             break;
         case 'j':// under
             current_player_pos[1]+=1;
+            record_history('k');
             break;
         case 'k':// top
             current_player_pos[1]-=1;
+            record_history('j');
             break;
         case 'l' :// right 
             current_player_pos[0]+=1;
+            record_history('h');
             break;
     }
     // Take a new picture of the golbaengi location
@@ -411,6 +416,43 @@ void newgame(void) // Restart from the first map
     printmap(imap);
 }
 
+int history_idx = 0; //* Variable that reads the history array backwards
+char history[5] = {'\0'}; //* Variable that records the movement command in reverse and stores 5 of them. Store to queue, read to stack.
+_Bool is_undoing = false; //* Variable to distinguish whether the player's movement is undo or a normal command
+
+void record_history(char move) //* Function to record player movements
+{
+    static int past_overwrite_cnt; //* A variable that determines whether history_idx will decrease during the record process.
+
+    if (!is_undoing && history_idx <= 4)
+    {
+        if (history_idx == 0)
+            past_overwrite_cnt = 0;
+
+        if (history_idx >= 0)
+            past_overwrite_cnt ++;
+
+        if (past_overwrite_cnt >= 4-history_idx || history_idx > 0)
+            history_idx --;
+
+        //! When a general command is input
+        for (int i = 0; i <= 4-history_idx; ++i)
+            history[i] = history[i+1];
+
+        history[4-history_idx] = move;
+    }
+}
+
+void undo(void)
+{
+    if (history_idx < 4 && history[4-history_idx] != '\0')
+    {
+        is_undoing = true;
+        decide_move(history[4-history_idx], current_map_no);
+
+        history_idx ++;
+    }
+}
 
 
 
@@ -439,12 +481,23 @@ int main(void)
     {
         //? Assume map file number 1, Map selection function scheduled to be added in the future
         command = getch();
+        printf("idx: %d\n", history_idx);
+        for (int i = 0; i <= 4; ++i) {
+            printf("%c", history[i]);
+        }
+        printf("\n");
+
         switch(command)
         {
             case 'n':
                 newgame();
                 break;
-        }        
+            case 'u':
+                undo();
+        }
+
+        is_undoing = false;
+
         decide_move(command, imap);
         printmap(current_map_no);
         // TESTING
